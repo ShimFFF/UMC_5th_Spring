14f5bf441e58cf4933700de5b731178d7dcb44df
ShimFFF <shimseawon1510@gmail.com>
2023-12-02 16:03:37 +0900

    :sparkles: Feat : @ExistCategories 커스텀 어노테이션 추가 #1

---
+++ project/spring/spring/src/main/java/umc/spring/Anotaion/ExistCategories.java
@@ -0,0 +1,19 @@
+package umc.spring.Anotaion;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Documented //사용자 정의 어노테이션을 만들 때
+@Constraint(validatedBy = CategoriesExistValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME) //어노테이션 정보를 언제까지 유지할 것인가 (생명 주기)
+public @interface ExistCategories {
+
+    String message() default "해당하는 카테고리가 존재하지 않습니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ project/spring/spring/src/main/java/umc/spring/Anotaion/Validator/CategoriesExistValidator.java
@@ -0,0 +1,37 @@
+package umc.spring.Anotaion.Validator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import umc.spring.Anotaion.ExistCategories;
+import umc.spring.ApiPayload.code.status.ErrorStatus;
+import umc.spring.repository.FoodCategRepository;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CategoriesExistValidator implements ConstraintValidator<ExistCategories, List<Long>> {
+
+    private final FoodCategRepository foodCategoryRepository;
+
+    @Override
+    public void initialize(ExistCategories constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
+        boolean isValid = values.stream()
+                .allMatch(value -> foodCategoryRepository.existsById(value));
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.FOOD_CATEGORY_NOT_FOUND.toString()).addConstraintViolation();
+        }
+
+        return isValid;
+
+    }
+}
